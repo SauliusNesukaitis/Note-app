8425ae3112f497cd7d4d41db21c674fceedfcb53
Saulius <sauliusnesukaitis@gmail.com>
2022-11-08 19:30:22 +0200

    Add password hashing in the user model

--- uzrasine.py
+++ uzrasine.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Regexp, EqualTo
 from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bootstrap import Bootstrap5
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -55,7 +56,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-   
+
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User %r>' % self.username
 
