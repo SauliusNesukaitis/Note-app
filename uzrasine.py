419cca40a1c2ccec5de8bf1d8c20b3b41b7b9b24
Saulius <sauliusnesukaitis@gmail.com>
2022-11-08 19:19:22 +0200

    Add user db model

--- uzrasine.py
+++ uzrasine.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Regexp, EqualTo
 from flask_wtf import FlaskForm
@@ -9,8 +10,11 @@ from flask_bootstrap import Bootstrap5
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
 app.config["SECRET_KEY"] = "1f54c168d2b78208b9d64c2a9664c03433fcef20"
 
+db = SQLAlchemy(app)
 bootstrap = Bootstrap5(app)
 
 
@@ -46,13 +50,23 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Register")
 
 
-@app.route("/", methods=["GET", "POST"])
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+   
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
-    return render_template("index.html", form=form)
+    return render_template('index.html', form=form)
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template("index.html", form=form)
+    return render_template('index.html', form=form)
