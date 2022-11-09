d0c0abe2e96943313a00e9d3c0ade6d321feb664
Saulius <sauliusnesukaitis@gmail.com>
2022-11-09 19:59:18 +0200

    Move forms to forms.py, add user loader

---
+++ forms.py
@@ -0,0 +1,35 @@
+from wtforms import SubmitField, StringField, BooleanField, PasswordField, ValidationError
+from wtforms.validators import DataRequired, Length, Regexp, EqualTo
+from flask_wtf import FlaskForm
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(1, 64)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Keep me logged in")
+    submit = SubmitField("Log in")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                '^[A-Za-z0-9][A-Za-z0-9_.]*$',
+                0,
+                'Usernames must have only letters, numbers, dots or ' 'underscores',
+            ),
+        ],
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password2', message='Passwords must match.'),
+        ],
+    )
+    password2 = PasswordField("Confirm password", validators=[DataRequired()])
+    submit = SubmitField("Register")
--- uzrasine.py
+++ uzrasine.py
@@ -1,14 +1,13 @@
 import os
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask_login import current_user, login_required, login_user, logout_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import SubmitField, StringField, PasswordField, ValidationError
-from wtforms.validators import DataRequired, Length, Regexp, EqualTo
-from flask_wtf import FlaskForm
+from wtforms import ValidationError
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap5
+from forms import LoginForm, RegistrationForm
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,45 +19,11 @@ app.config["SECRET_KEY"] = "1f54c168d2b78208b9d64c2a9664c03433fcef20"
 db = SQLAlchemy(app)
 bootstrap = Bootstrap5(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
 
 
-class LoginForm(FlaskForm):
-    username = StringField(
-        "Username", validators=[DataRequired(), Length(1, 64)]
-    )
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Log in")
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField(
-        "Username",
-        validators=[
-            DataRequired(),
-            Length(1, 64),
-            Regexp(
-                '^[A-Za-z0-9][A-Za-z0-9_.]*$',
-                0,
-                'Usernames must have only letters, numbers, dots or ' 'underscores',
-            ),
-        ],
-    )
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            EqualTo('password2', message='Passwords must match.'),
-        ],
-    )
-    password2 = PasswordField("Confirm password", validators=[DataRequired()])
-    submit = SubmitField("Register")
-
-    def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError("Username already in use.")
-
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
@@ -86,6 +51,11 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -93,6 +63,8 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data.lower()).first()
@@ -100,7 +72,7 @@ def login():
             login_user(user, form.remember_me.data)
             next = request.args.get("next")
             if next is None or not next.startswith("/"):
-                next = url_for("main.index")
+                next = url_for("index")
             return redirect(next)
     return render_template("login.html", form=form)
 
